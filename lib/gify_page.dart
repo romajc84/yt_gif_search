97e3f6b642d11be33234309d2a02b0290336a369
romajc <joeyroman84@gmail.com>
2021-06-27 17:05:38 -0400

    final update

--- lib/gify_page.dart
+++ lib/gify_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -15,11 +16,23 @@ class _GifyPageState extends State<GifyPage> {
       'https://api.giphy.com/v1/gifs/search?api_key=pe3ZWyViWslU9f87eJGnEdq3vg4gYkJ0&limit=25&offset=0&rating=G&lang=en&q=';
   var data;
 
-  getData(String searchInput) async {
-    var res = await http.get(Uri.parse(url + searchInput));
+  bool showLoading = false;
+
+  @override
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  getData(String searchText) async {
+    showLoading = true;
+    setState(() {});
+    final res = await http.get(Uri.parse(url + searchText));
     data = jsonDecode(res.body)["data"];
 
-    setState(() {});
+    setState(() {
+      showLoading = false;
+    });
   }
 
   @override
@@ -29,56 +42,73 @@ class _GifyPageState extends State<GifyPage> {
       body: SafeArea(
         child: Theme(
           data: ThemeData.dark(),
-          child: Column(
-            children: [
-              'Gify App'.text.white.xl4.make().objectCenter(),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: controller,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Search here',
+          child: Column(children: [
+            'Gify App'.text.white.xl4.make().objectCenter(),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Search here',
+                    ),
+                  ),
+                ),
+                30.widthBox,
+                ElevatedButton(
+                  onPressed: () {
+                    getData(controller.text);
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
                   ),
-                  30.widthBox,
-                  ElevatedButton(
-                    onPressed: () {
-                      getData(controller.text);
-                    },
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                    child: Text('Go'),
-                  ).h8(context),
-                ],
-              ).p8(),
+                  child: Text('Go'),
+                ).h8(context),
+              ],
+            ).p8(),
+            if (showLoading)
+              CircularProgressIndicator().centered()
+            else
               VxConditional(
                 condition: data != null,
                 builder: (context) => GridView.builder(
+                  shrinkWrap: true,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: context.isMobile ? 2 : 3,
                   ),
                   itemBuilder: (context, index) {
-                    final url =
+                    final imgUrl =
                         data[index]["images"]["fixed_height"]["url"].toString();
-                    return Image.network(
-                      url,
-                      fit: BoxFit.cover,
-                    ).card.roundedSM.make();
+                    return ZStack(
+                      [
+                        BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                          child: Image.network(
+                            imgUrl,
+                            fit: BoxFit.cover,
+                            color: Colors.black.withOpacity(0.8),
+                            colorBlendMode: BlendMode.darken,
+                          ),
+                        ),
+                        Image.network(
+                          imgUrl,
+                          fit: BoxFit.contain,
+                        )
+                      ],
+                      fit: StackFit.expand,
+                    ).card.roundedSM.make().p4();
                   },
                   itemCount: data.length,
                 ),
-                fallback: (context) => 'Nothing found'.text.gray500.xl3.make(),
-              ).h(context.percentHeight * 70),
-            ],
-          ).p16(),
+                fallback: (context) =>
+                    "Nothing found".text.gray500.xl3.makeCentered(),
+              ).h(context.percentHeight * 70)
+          ]).p16().scrollVertical(physics: NeverScrollableScrollPhysics()),
         ),
       ),
     );
