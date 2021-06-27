4803171fb8141463fad4ccd3683cd8c97c6995e6
romajc <joeyroman84@gmail.com>
2021-06-27 07:01:04 -0400

    first commit

---
+++ lib/gify_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GifyPage extends StatefulWidget {
+  @override
+  _GifyPageState createState() => _GifyPageState();
+}
+
+class _GifyPageState extends State<GifyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'gify_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Gify',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: GifyPage(),
+    );
+  }
+}
